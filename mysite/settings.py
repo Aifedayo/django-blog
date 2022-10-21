46528581a3e597eaecace15b86021cc715758674
aifedayo <akeemifedayolag@gmail.com>
2022-10-21 23:31:57 +0100

    .gitignore is now working

--- mysite/settings.py
+++ mysite/settings.py
@@ -122,3 +122,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'hakeempythonclass@gmail.com'
+EMAIL_HOST_PASSWORD = 'Adezeenab01'
